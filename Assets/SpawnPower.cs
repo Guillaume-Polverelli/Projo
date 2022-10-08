6746f48d9fa8f77ae8a60d9a7e08e0b8942bb894
Romain DESMARECAUX <78937225+RomainDes@users.noreply.github.com>
2022-10-08 06:44:51 -0400

    rhh
    rezh

---
+++ Assets/SpawnPower.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPower : MonoBehaviour
+{
+    public GameObject[] powers;
+    public GameObject[] _listpospowers;
+    public GameObject _pospower;
+    public GameObject power;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartPowers());
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator PowerAppears()
+    {
+        _listpospowers = GameObject.FindGameObjectsWithTag("Power");
+        int index = Random.Range(0, _listpospowers.Length);
+        _pospower = _listpospowers[index];
+
+        int index2 = Random.Range(0, powers.Length);
+        power = powers[index];
+
+        power = Instantiate(power, _pospower.transform.position, Quaternion.identity, this.transform);
+        yield return new WaitForSeconds(20);
+        if (power)
+        {
+            Destroy(power);
+        }
+        
+        StartCoroutine(PowerAppears());
+
+    }
+
+    private IEnumerator StartPowers()
+    {
+        yield return new WaitForSeconds(20);
+        StartCoroutine(PowerAppears());
+    }
+}
