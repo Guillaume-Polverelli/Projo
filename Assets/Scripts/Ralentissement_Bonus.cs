5bc776d680d6b49cefbfa64951b82f6001adb64d
Romain DESMARECAUX <78937225+RomainDes@users.noreply.github.com>
2022-10-09 12:50:15 -0400

    evz
    vzervz

--- Assets/Scripts/Ralentissement_Bonus.cs
+++ Assets/Scripts/Ralentissement_Bonus.cs
@@ -4,53 +4,32 @@ using UnityEngine;
 
 public class Ralentissement_Bonus : MonoBehaviour
 {
-    [SerializeField] private float _delay;
-    [SerializeField] private float _slowMultiplier;
-    private bool _active;
-    private float _startTime;
+
     private GameObject[] players;
 
     // Start is called before the first frame update
     void Start()
     {
-        _active = false;
+      
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            _active = true;
-            _startTime = Time.fixedTime;
-            gameObject.SetActive(false);
+            collision.gameObject.GetComponent<PlayerMovement>().ralentissement = true;
+
+            Destroy(gameObject);
             // Animation fadeout
 
-            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-            foreach (GameObject player in players)
-            {
-                if (collision.gameObject.GetComponent<PlayerMovement>().get_player() != player.GetComponent<PlayerMovement>().get_player() && player.GetComponent<BoxCollider2D>().enabled)
-                {
-                    player.GetComponent<PlayerMovement>().set_speed(player.GetComponent<PlayerMovement>().get_speed() / _slowMultiplier);
-                    player.GetComponent<PlayerMovement>().isSlowed = true;
-                }
-            }
+            
         }
     }
 
     void Update()
     {
-        if (_active && Time.fixedTime - _startTime > _delay)
-        {
-            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-            foreach (GameObject player in players)
-            {
-                if (player.GetComponent<BoxCollider2D>().enabled && player.GetComponent<PlayerMovement>().isSlowed == true)
-                {
-                    player.GetComponent<PlayerMovement>().set_speed(player.GetComponent<PlayerMovement>().get_speed() * _slowMultiplier);
-                }
-            }
-
-            Destroy(gameObject);
-        }
+        
     }
+
+    
 }
