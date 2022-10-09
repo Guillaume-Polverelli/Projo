4c5148ad378603351cd4f2b56f575dc0fbc60e96
Camille <camille.boucher@imt-atlantique.net>
2022-10-09 11:15:56 -0400

    go from menu to play

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Test_Camille");
     }
     public void QuitGame()
     {
